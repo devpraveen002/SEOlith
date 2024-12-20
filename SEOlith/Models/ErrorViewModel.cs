94e25f1c08d216a2fa5b113b0e48fda3806abf8c
devpraveen002 <dev.praveen002@gmail.com>
2024-12-21 01:40:40 +0530

    Initial create

---
+++ SEOlith/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SEOlith.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
